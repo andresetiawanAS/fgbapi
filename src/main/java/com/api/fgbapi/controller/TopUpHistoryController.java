b35dbb83e5438a3c9ccc3be5ec9515c6e5eae1e6
Kristoforus M <faytmartin18@gmail.com>
2018-07-24 16:20:40 +0700

    history topup
    dikit

--- src/main/java/com/api/fgbapi/controller/TopUpHistoryController.java
+++ src/main/java/com/api/fgbapi/controller/TopUpHistoryController.java
@@ -1,4 +1,27 @@
 package com.api.fgbapi.controller;
 
+import com.api.fgbapi.model.Balance;
+import com.api.fgbapi.model.TopUpHistory;
+import com.api.fgbapi.service.BalanceService;
+import com.api.fgbapi.service.TopUpHistoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+@RequestMapping(path = "/history", method = GET)
 public class TopUpHistoryController {
+
+    @Autowired
+    TopUpHistoryService topUpHistoryService;
+
+    @GetMapping(value = "/topup")
+    public List<TopUpHistory> getAll() {
+        return topUpHistoryService.findAll();
+    }
 }
--- src/main/java/com/api/fgbapi/repository/TourListRepository.java
+++ src/main/java/com/api/fgbapi/repository/TourListRepository.java
@@ -1,6 +1,5 @@
 package com.api.fgbapi.repository;
 
-import com.api.fgbapi.model.TourList;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface TourListRepository extends MongoRepository<TourList, String> {
