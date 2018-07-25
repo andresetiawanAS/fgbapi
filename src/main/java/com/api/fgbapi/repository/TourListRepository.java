5e5a3b67e5495565b462111976966894346d7739
dennis <dennis.kusuma@student.umn.ac.id>
2018-07-25 11:35:41 +0700

    tourlist
    test

--- src/main/java/com/api/fgbapi/repository/TourListRepository.java
+++ src/main/java/com/api/fgbapi/repository/TourListRepository.java
@@ -1,5 +1,6 @@
 package com.api.fgbapi.repository;
 
+import com.api.fgbapi.model.TourList;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface TourListRepository extends MongoRepository<TourList, String> {
